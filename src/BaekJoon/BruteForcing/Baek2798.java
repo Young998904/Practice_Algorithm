2c9ae6cde0f746e15f665e09c41628db3e2cc876
Young998904 <like0998904@gmail.com>
2022-09-14 17:07:23 +0900

    :memo: Baek2798 블랙잭

---
+++ src/BaekJoon/BruteForcing/Baek2798.java
@@ -0,0 +1,49 @@
+package BaekJoon.BruteForcing;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Baek2798 {
+    public static void main(String[] args) throws IOException {
+        // N, M, numList 입력 받음
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> numList = new ArrayList<>();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i=0; i < N; i++) {
+            numList.add(Integer.parseInt(st.nextToken()));
+        }
+
+        // 세 합 비교
+        int result = 0;
+
+        for (int i=0; i < N-2; i++) {
+            for (int k=i+1; k < N-1; k++) {
+                for (int j=k+1; j < N; j++) {
+                    int total = numList.get(i) + numList.get(k) + numList.get(j);
+
+                    if (total == M) {
+                        System.out.println(M);
+                        return;
+                    }
+                    if (total > M) {
+                        continue;
+                    }
+                    else {
+                        result = (M-total < M-result) ? total : result;
+                    }
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
