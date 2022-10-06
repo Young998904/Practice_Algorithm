8facf9c29bb8151482f33fdb9318aa8a1598af2a
Young998904 <like0998904@gmail.com>
2022-10-06 17:28:51 +0900

    :memo: Baek10870 피보나치 수 5 (함수 호출 방식 추가)

--- src/BaekJoon/Step/Recursion/Baek10870.java
+++ src/BaekJoon/Step/Recursion/Baek10870.java
@@ -2,6 +2,7 @@ package BaekJoon.Step.Recursion;
 
 import java.util.Scanner;
 
+// 방법 1
 public class Baek10870 {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -28,3 +29,33 @@ public class Baek10870 {
         return one + two;
     }
 }
+
+//// 방법 2
+//public class Baek10870 {
+//    public int fibo(int one, int two) {
+//        return one + two;
+//    }
+//    public static void main (String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int N = sc.nextInt();
+//
+//        Baek10870 baek10870 = new Baek10870();
+//
+//        // 0 이나 1 일 때
+//        if (N == 0 || N == 1) {
+//            System.out.println(N);
+//            return;
+//        }
+//
+//        int one = 0;
+//        int two = 1;
+//        int newOne;
+//
+//        for (int i=2; i<=N; i++) {
+//            newOne = two;
+//            two = baek10870.fibo(one, two);
+//            one = newOne;
+//        }
+//        System.out.println(two);
+//    }
+//}
